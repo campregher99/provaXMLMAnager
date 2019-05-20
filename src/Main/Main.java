f1c4de6207dec6bd3d922b4869d8086de4ac8891
Daniele <28843028+Chiaf1@users.noreply.github.com>
2019-05-20 22:17:39 +0200

    implementazione nuova fuzione confronto tra strutture dati

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,9 +7,16 @@ public class Main {
 	private static DecodificatoreXML dec=new DecodificatoreXML();
 	public static void main(String[] args) {
 		
-		dec.leggiFile("C:\\Users\\dchia\\Documents\\_GitHub\\provaXMLMAnager\\PgAr_Map_5.xml");
+		dec.leggiFile("C:\\Users\\dchia\\Documents\\_GitHub\\provaXMLMAnager\\codiciFiscali.xml");
 		StrutturaDati nuovo0 = dec.getFile();
 		dec.scriviFile(nuovo0, "bello", "utf-8", "1.0");
+		dec.leggiFile("C:\\Users\\dchia\\Documents\\_GitHub\\provaXMLMAnager\\bello.xml");
+		StrutturaDati nuovo1 = dec.getFile();
+		if(dec.confronto(nuovo0, nuovo1)){
+			System.out.println("Le due strutture dati sono uguali");
+		}else {
+			System.out.println("Le due strutture datu sono diverse");
+		}
 	}
 
 }
--- src/XMLManager/DecodificatoreXML.java
+++ src/XMLManager/DecodificatoreXML.java
@@ -1,5 +1,7 @@
 package XMLManager;
 
+import java.util.Map.Entry;
+
 public class DecodificatoreXML {
 	private LetturaScrittura inputFile = new LetturaScrittura();
 
@@ -24,5 +26,32 @@ public class DecodificatoreXML {
 		if (inputFile.scriviFile(input, nomeFile, formato, versione))
 			return true;
 		return false;
+	}
+	
+	public boolean confronto(StrutturaDati in1, StrutturaDati in2) {
+		if(!in1.getNome().equals(in2.getNome())) {
+			return false;
+		}
+		if(in1.getTag().size() != in2.getTag().size()) {
+			return false;
+		}
+		for (Entry<String, String> tag : in1.getTag().entrySet()) {
+			try {
+				if(!in1.getTag(tag.getKey()).equals(in2.getTag(tag.getKey()))){
+					return false;
+				}
+			}catch(Exception e){
+				return false;
+			}
+		}
+		if(in1.getAttributi().size() != in2.getAttributi().size()) {
+			return false;
+		}	
+		for (int i = 0; i<in1.getAttributi().size(); i++) {
+			if(!confronto(in1.getAttributi().get(i),in2.getAttributi().get(i))) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
